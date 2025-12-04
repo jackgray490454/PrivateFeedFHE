af76b1678ae116cf167bc92a0a2f7d7633d712a6
jackgray490454 <semen.s53164860@rambler.ru>
2025-12-04 19:12:11 +0800

    add mock-common.java, package.json, config.json

---
+++ frontend/web/src/global/adapter-gateway-base.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\global\adapter-gateway-base.go
+// Generated at: 2025-10-13T08:11:15.701Z
---
+++ frontend/web/src/mutations-manager/mock-common.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\mutations-manager\mock-common.java
+// Generated at: 2025-10-13T08:11:15.708Z
