d9fbea6f3ea31ef7973ec4267f3080a73bb2bbcf
jackgray490454 <semen.s53164860@rambler.ru>
2025-12-04 19:11:42 +0800

    add config-download-decorator.go, analytics-report.ts, f5704efdb74ab253a009e5f8253c3973.json

---
+++ frontend/web/src/config-download-decorator.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\config-download-decorator.go
+// Generated at: 2025-10-13T08:11:15.697Z
---
+++ template.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\template.java
+// Generated at: 2025-10-13T08:11:15.688Z
